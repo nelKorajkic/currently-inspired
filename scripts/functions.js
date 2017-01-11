b026c1906d05fd5d93459bdc20ec38ff1e63801d
Nel Korajkic <nelkorajkic@gmail.com>
2017-01-11 02:01:40 -0500

    got the time displaying, still isn't perfectly vertically alligned

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,1 +1,19 @@
-var date = new Date();
+function timecheck() {
+  var date = new Date();
+  var hour = date.getHours();
+  console.log(hour);
+  var minute = date.getMinutes();
+  console.log(minute);
+  var second = date.getSeconds();
+  console.log(second);
+  document.getElementById("hourDisplay").innerHTML = hour + ":";
+  document.getElementById("minuteDisplay").innerHTML = minute + ":";
+  document.getElementById("secondDisplay").innerHTML = second;
+}
+var looper =setInterval(timecheck,1000);
+window.onload =timecheck;
+//window.onload = bgSetter;
+function bgSetter(){
+  var bgPicked;
+  document.getElementById("bg").style.backgroundColor = bgPicked;
+}
