9fddb2ee8a93eb53d4b06db0cb7cd014b83f19b7
Nel Korajkic <nelkorajkic@gmail.com>
2017-01-12 03:12:03 -0500

    Got the sidebar working which includes the background picker, used an imperfect method of determining font color based on bg color, might rework later on, thinking of a section to display the date as well.

--- scripts/functions.js
+++ scripts/functions.js
@@ -6,6 +6,9 @@ function timecheck() {
   console.log(minute);
   var second = date.getSeconds();
   console.log(second);
+  if (hour<10){
+    hour = "0" +hour;
+  }
   if (minute<10){
     minute = "0" +minute;
   }
@@ -17,7 +20,51 @@ function timecheck() {
 var looper =setInterval(timecheck,1000);
 window.onload =timecheck;
 //window.onload = bgSetter;
-function bgSetter(){
-  var bgPicked;
-  document.getElementById("bg").style.backgroundColor = bgPicked;
+
+
+$('img#imgbtn').click(function() {
+  console.log("memes");
+  $('.sidebar').slideDown(300);
+});
+
+$('img#closeBtn','div').click(function() {
+  console.log("memes");
+  $('.sidebar').slideUp(300);
+});
+
+$('.colorContainer div').click(function() {
+  var bgPicked = $(this).css('backgroundColor');
+  console.log(bgPicked);
+    var parts = bgPicked.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+    delete(parts[0]);
+    for (var i = 1; i <= 3; ++i) {
+        parts[i] = parseInt(parts[i]).toString(16);
+        if (parts[i].length == 1) parts[i] = '0' + parts[i];
+    }
+    bgPicked = '#' + parts.join('');
+    bgSetter(bgPicked);
+});
+
+function bgSetter(bgPicked){
+  console.log(bgPicked);
+  document.getElementById("bg").style.backgroundColor = (bgPicked);
+  bgPicked = bgPicked.slice(1,6);
+  hexToRgb(bgPicked);
+}
+
+function hexToRgb(bgPicked) {
+    console.log(bgPicked);
+    var bigint = parseInt(bgPicked, 16);
+    var r = (bigint >> 16) & 255;
+    var g = (bigint >> 8) & 255;
+    var b = bigint & 255;
+
+    //perceptive luminance - human eye favor of the color green
+    var a = 1 - ( (0.299 * r) + (0.587 * g) + (0.114 * b))/255;
+    if (a < 0.5)
+      document.getElementById("time").style.color = "#444444"; //bright colors - black font
+    else {
+      document.getElementById("time").style.color = "#DDDDDD"; //dark colors - white font
+    }
+    return ;
 }
