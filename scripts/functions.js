910ae4080efd52948de4fe3bf8b278023abe9651
Nel Korajkic <nelkorajkic@gmail.com>
2017-01-12 21:45:44 -0500

    So I got the cookies working to keep the font color when you leave the page and come back BUT can't get the background color to change, code is super sloppy because I was testing stuff but I might need to roll back to this if I ruin something

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,11 +1,8 @@
 function timecheck() {
   var date = new Date();
   var hour = date.getHours();
-  console.log(hour);
   var minute = date.getMinutes();
-  console.log(minute);
   var second = date.getSeconds();
-  console.log(second);
   if (hour<10){
     hour = "0" +hour;
   }
@@ -15,7 +12,6 @@ function timecheck() {
   if (second<10){
     second = "0" +second;
   }
-
   var month = date.getMonth() + 1;
   if (month<10) {
     month = "0" +month;
@@ -29,11 +25,27 @@ function timecheck() {
   document.title = hour + ":"+minute + ":"+second;
   document.getElementById("date").innerHTML = month + ":"+day + ":"+year;
 }
+
 var looper =setInterval(timecheck,1000);
 window.onload =timecheck;
-//window.onload=hexToRgb();
-//var x = document.cookie;
 
+var x = document.cookie;
+String(x);
+x = x.slice(9,16);
+//console.log(x);
+bgPicked = x;
+hexToRgb(bgPicked);
+
+// readCookie(x);
+// function readCookie(name) {
+// 	var nameEQ = name + "=";
+// 	var ca = document.cookie.split(';');
+// 	for(var i=0;i < ca.length;i++) {
+// 		var c = ca[i];
+// 		while (c.charAt(0)==' ') c = c.substring(1,c.length);
+// 		if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length,c.length);
+// 	}
+// }
 $('img#imgbtn').click(function() {
   $('.sidebar').slideDown(300);
   $('#overlay').show(3);
@@ -50,7 +62,7 @@ $('#overlay').click(function() {
 
 $('.colorContainer div').click(function() {
   var bgPicked = $(this).css('backgroundColor');
-  console.log(bgPicked);
+  //console.log(bgPicked);
     var parts = bgPicked.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
     delete(parts[0]);
     for (var i = 1; i <= 3; ++i) {
@@ -63,17 +75,23 @@ $('.colorContainer div').click(function() {
 
 function bgSetter(bgPicked){
   console.log(bgPicked);
-  document.getElementById("bg").style.backgroundColor = (bgPicked);
-  bgPicked = bgPicked.slice(1,6);
+  document.getElementById("bg").style.backgroundColor = bgPicked;
+  console.log("did this happen?");
   hexToRgb(bgPicked);
 }
 
 function hexToRgb(bgPicked) {
+    console.log(bgPicked);
+    var x = document.getElementById("bg").style.backgroundColor = bgPicked;
+    console.log(x);
+    bgPicked = bgPicked.slice(1,6);
+    document.cookie ='bgCookie=#'+ bgPicked;
     var bigint = parseInt(bgPicked, 16);
     var r = (bigint >> 16) & 255;
     var g = (bigint >> 8) & 255;
     var b = bigint & 255;
-
+    bgPicked = "#" + bgPicked;
+    console.log(bgPicked);
     //perceptive luminance - human eye favor of the color green
     var a = 1 - ( (0.299 * r) + (0.587 * g) + (0.114 * b))/255;
     if (a < 0.5) {
@@ -86,4 +104,5 @@ function hexToRgb(bgPicked) {
       document.getElementById("time").style.color = "#DDDDDD";
       document.getElementById("date").style.color ="#CCCCCC";
     }
+    //return document.cookie;
 }
